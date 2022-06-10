8f042cd84e9cf794b0ea07fa41db02e4545d37d2
zahrafitriani <80316854+zahrafitriani@users.noreply.github.com>
2022-06-10 22:34:23 +0700

    Add files via upload

--- Plotly_Bar_Chart.js
+++ Plotly_Bar_Chart.js
@@ -1,11 +1,15 @@
+//Untuk mengambil id dari html
 BAR = document.getElementById('barplot');
 
+//Masukkan dataset untuk sumbu x
 var xValue = ['Denmark', 'United Kingdom', 'Saudi Arabia', 'Brazil', 'Kazakhstan', 'Mauritius', 'Argentina', 'Philippines', 'Moldova', 'Hong Kong', 'Bulgaria', 'Libya', 'Liberia', 'Congo', 'Georgia', 'Venezuela', 'Kenya', 'Myanmar', 'Chad', 'Lebanon'];
 
+//Masukkan dataset untuk sumbu y
 var yValue = [7636, 6943, 6523, 6293, 6234, 6071, 5967, 5904, 5857, 5425, 5371, 5330, 5122, 5075, 4973, 4925, 4543, 4394, 4251, 2955];
 var yValue2 = [1953, 1867, 1870, 1462, 1668, 1591, 1592, 1268, 1417, 1957, 1625, 1476, 636, 950, 1467, 1844, 1032, 1038, 662, 1392];
 var yValue3 = [532, 329, 180, 134, 157, 107, 85, 142, 28, 383, 21, 179, 80, 146, 208, 82, 82, 194, 77, 34]
 
+//Membuat bar chart atribut Happines score
 var Happiness_Score = {
     x: xValue,
     y: yValue,
@@ -14,6 +18,7 @@ var Happiness_Score = {
     text: yValue.map(String),
     textposition: 'auto',
     hoverinfo: 'none',
+    //Melakukan pewarnaan terhadap bar chart
     marker: {
         color: '#044bd9',
         line: {
@@ -23,6 +28,7 @@ var Happiness_Score = {
     }
 };
 
+//Membuat bar chart atribut GDP per Capita
 var GDP_per_Capita = {
     x: xValue,
     y: yValue2,
@@ -31,6 +37,7 @@ var GDP_per_Capita = {
     text: yValue2.map(String),
     textposition: 'auto',
     hoverinfo: 'none',
+    //Melakukan pewarnaan terhadap bar chart
     marker: {
         color: '#6eb5ff',
         line: {
@@ -40,6 +47,7 @@ var GDP_per_Capita = {
     }
 };
 
+//Membuat bar chart atribut Perception of Corruption
 var Perception_of_Corruption = {
     x: xValue,
     y: yValue3,
@@ -48,6 +56,7 @@ var Perception_of_Corruption = {
     text: yValue2.map(String),
     textposition: 'auto',
     hoverinfo: 'none',
+    //Melakukan pewarnaan terhadap bar chart
     marker: {
         color: '#1dddf2',
         line: {
@@ -57,10 +66,15 @@ var Perception_of_Corruption = {
     }
 };
 
+//Memanggil fungsi untuk yang akan dijadikan bar chart
 var data = [Happiness_Score, GDP_per_Capita, Perception_of_Corruption];
 
+//Membuat judul
 var layout = {
     title: 'Tingkat Kebahagiaan Dunia Berdasarkan World Happiness Score, GDP per Capita dan Perception of Corruption di Beberapa Negara'
 };
+
+//Mengkonfigurasikan agar web menjadi responsif
 var config = { responsive: true };
+//Untuk menggabungkan semua elemen yang ada
 Plotly.newPlot(BAR, data, layout, config);
--- Plotly_Line_Chart.js
+++ Plotly_Line_Chart.js
@@ -1,9 +1,15 @@
+//Untuk mengambil id dari html
 LINESCATTER = document.getElementById('linescatter');
+
+//Membuat line chart atribut Happines score
 var trace1 = {
+    //Masukkan dataset untuk sumbu x
     x: ['Denmark', 'United Kingdom', 'Saudi Arabia', 'Brazil', 'Kazakhstan', 'Mauritius', 'Argentina', 'Philippines', 'Moldova', 'Hong Kong', 'Bulgaria', 'Libya', 'Liberia', 'Congo', 'Georgia', 'Venezuela', 'Kenya', 'Myanmar', 'Chad', 'Lebanon'],
+    //Masukkan dataset untuk sumbu y
     y: [7636, 6943, 6523, 6293, 6234, 6071, 5967, 5904, 5857, 5425, 5371, 5330, 5122, 5075, 4973, 4925, 4543, 4394, 4251, 2955],
     type: 'scatter',
     name: 'World Happiness Score',
+    //Melakukan pewarnaan terhadap line chart
     marker: {
         color: '#044bd9',
         line: {
@@ -13,11 +19,15 @@ var trace1 = {
     }
 };
 
+//Membuat line chart atribut GDP per Capita
 var trace2 = {
+    //Masukkan dataset untuk sumbu x
     x: ['Denmark', 'United Kingdom', 'Saudi Arabia', 'Brazil', 'Kazakhstan', 'Mauritius', 'Argentina', 'Philippines', 'Moldova', 'Hong Kong', 'Bulgaria', 'Libya', 'Liberia', 'Congo', 'Georgia', 'Venezuela', 'Kenya', 'Myanmar', 'Chad', 'Lebanon'],
+    //Masukkan dataset untuk sumbu y
     y: [1953, 1867, 1870, 1462, 1668, 1591, 1592, 1268, 1417, 1957, 1625, 1476, 636, 950, 1467, 1844, 1032, 1038, 662, 1392],
     type: 'scatter',
     name: 'GDP per Capita',
+    //Melakukan pewarnaan terhadap line chart
     marker: {
         color: '#6eb5ff',
         line: {
@@ -27,11 +37,15 @@ var trace2 = {
     }
 };
 
+//Membuat line chart atribut Perception of Corruption
 var trace3 = {
+    //Masukkan dataset untuk sumbu x
     x: ['Denmark', 'United Kingdom', 'Saudi Arabia', 'Brazil', 'Kazakhstan', 'Mauritius', 'Argentina', 'Philippines', 'Moldova', 'Hong Kong', 'Bulgaria', 'Libya', 'Liberia', 'Congo', 'Georgia', 'Venezuela', 'Kenya', 'Myanmar', 'Chad', 'Lebanon'],
+    //Masukkan dataset untuk sumbu y
     y: [532, 329, 180, 134, 157, 107, 85, 142, 28, 383, 21, 179, 80, 146, 208, 82, 82, 194, 77, 34],
     type: 'scatter',
     name: 'Perception of Corruption',
+    //Melakukan pewarnaan terhadap line chart
     marker: {
         color: '#1dddf2',
         line: {
@@ -41,10 +55,15 @@ var trace3 = {
     }
 };
 
+//Memanggil fungsi untuk yang akan dijadikan line chart
 var data = [trace1, trace2, trace3];
+
+//Membuat judul
 var layout = {
     title: 'Tingkat Kebahagiaan Dunia Berdasarkan World Happiness Score, GDP per Capita dan Perception of Corruption di Beberapa Negara'
 };
 
+//Mengkonfigurasikan agar web menjadi responsif
 var config = { responsive: true };
+//Untuk menggabungkan semua elemen yang ada
 Plotly.newPlot(LINESCATTER, data, layout, config);
